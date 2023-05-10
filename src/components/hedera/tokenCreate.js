3a50052664b874995882089b83fef109eea809eb
sabya <sabya7011@gmail.com>
2023-05-10 17:29:24 +0530

    Pair wallet and create FT

---
+++ src/components/hedera/tokenCreate.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { TokenCreateTransaction, PublicKey, TokenSupplyType, TokenType } from "@hashgraph/sdk";
+
+async function tokenCreateFcn(walletData, accountId) {
+	console.log(`\n=======================================`);
+	console.log(`- Creating HTS token...`);
+
+	const hashconnect = walletData[0];
+	const saveData = walletData[1];
+	const provider = hashconnect.getProvider("testnet", saveData.topic, accountId);
+	const signer = hashconnect.getSigner(provider);
+
+	const url = `https://testnet.mirrornode.hedera.com/api/v1/accounts?account.id=${accountId}`;
+	const mirrorQuery = await axios(url);
+	const supplyKey = PublicKey.fromString(mirrorQuery.data.accounts[0].key.key);
+
+	const tokenCreateTx = await new TokenCreateTransaction()
+		.setTokenName("Carbon Credit")
+		.setTokenSymbol("CC")
+		.setTreasuryAccountId(accountId)
+		.setAutoRenewAccountId(accountId)
+		.setAutoRenewPeriod(7776000)
+		.setTokenType(TokenType.FungibleCommon)
+        .setInitialSupply(1000)
+        .setSupplyType(TokenSupplyType.Finite)
+        .setMaxSupply(2000)
+		.setDecimals(0)
+		.setSupplyKey(supplyKey)
+		.freezeWithSigner(signer);
+	const tokenCreateSubmit = await tokenCreateTx.executeWithSigner(signer);
+	const tokenCreateRx = await provider.getTransactionReceipt(tokenCreateSubmit.transactionId);
+	const tId = tokenCreateRx.tokenId;
+	const supply = tokenCreateTx._initialSupply.low;
+	console.log(`- Created HTS token with ID: ${tId}`);
+
+	return [tId, supply, tokenCreateSubmit.transactionId];
+}
+
+export default tokenCreateFcn;
